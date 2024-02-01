f21e4df99ad29640d8b45a8f47fb5710ef4486ca
orhan <orhantrkmn749@gmail.com>
2024-02-01 22:03:23 +0300

    swagger3 documantation added.

--- src/main/java/com/example/webMvcCrudDemo/controller/BookController.java
+++ src/main/java/com/example/webMvcCrudDemo/controller/BookController.java
@@ -3,9 +3,12 @@ package com.example.webMvcCrudDemo.controller;
 import com.example.webMvcCrudDemo.dto.BookDto;
 import com.example.webMvcCrudDemo.model.Book;
 import com.example.webMvcCrudDemo.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +25,8 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping("/books")
+    @Operation(summary = "list books", description = "list books")
+    @ApiResponse(responseCode = "200",description = "list books")
     public String getBookList(Model model) {
         List<BookDto> getBookList = bookService.getBookList();
         model.addAttribute("books", getBookList);
@@ -29,6 +34,8 @@ public class BookController {
     }
 
     @GetMapping("/books/create")
+    @Operation(summary = "create book", description = "create the book object")
+    @ApiResponse(responseCode = "201",description = "create book")
     public String createBookForm(Model model) {
         BookDto book = new BookDto();
         model.addAttribute("books", book);
@@ -36,7 +43,9 @@ public class BookController {
     }
 
     @PostMapping("/books/create")
-    public String saveBook(@Valid @ModelAttribute("books") Book book
+    @Operation(summary = "create book", description = "create book")
+     @ApiResponse(responseCode = "201",description = "create book")
+    public String saveBook( @Valid @ModelAttribute("books")  Book book
             , BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
@@ -48,6 +57,8 @@ public class BookController {
     }
 
     @GetMapping("/books/{bookId}/update")
+    @Operation(summary = "update book", description = "update book")
+    @ApiResponse(responseCode = "201",description = "update book")
     public String updateBookForm(@PathVariable("bookId") long bookId, Model model) {
         BookDto bookDto = bookService.findBookById(bookId);
         model.addAttribute("books", bookDto);
@@ -55,6 +66,8 @@ public class BookController {
     }
 
     @PostMapping("/books/{bookId}/update")
+    @Operation(summary = "update book", description = "update book")
+    @ApiResponse(responseCode = "201",description = "update book")
     public String updateBook(@PathVariable("bookId") long bookId, @ModelAttribute("books") Book books) {
         books.setId(bookId);
         bookService.saveBook(books);
@@ -62,11 +75,12 @@ public class BookController {
     }
 
     @GetMapping("/books/{bookId}/delete")
+    @Operation(summary = "delete book object by id", description = "delete book")
+    @ApiResponse(responseCode = "200",description = "Delete book")
     public String deleteBook(@PathVariable("bookId") long bookId, Model model) {
         bookService.deleteBookById(bookId);
         model.addAttribute("books", bookId);
         return "redirect:/books";
     }
-
 
 }
--- src/main/java/com/example/webMvcCrudDemo/dto/BookDto.java
+++ src/main/java/com/example/webMvcCrudDemo/dto/BookDto.java
@@ -1,5 +1,6 @@
 package com.example.webMvcCrudDemo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,49 +15,119 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(title = "books", description = "Book model")
 public class BookDto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(
+            description = "id of the Book",
+            name = "id",
+            type = "long",
+            example = "1")
     private Long id;
 
+    @Schema(
+            description = "Name of the Book",
+            name = "name",
+            type = "String",
+            example = "Var Mısın ?")
     @NotNull(message = " must not be empty name")
     private String name;
 
+    @Schema(
+            description = "Barcode of the Book",
+            name = "barcode",
+            type = "String",
+            example = "9786057635839")
     @NotNull(message = " must not be empty barcode")
-    @Min(value = 0 ,message = "must be greater than or equal to zero")
+    @Min(value = 0, message = "must be greater than or equal to zero")
     private String barcode;
 
+
+    @Schema(
+            description = "Brand of the Book",
+            name = "brand",
+            type = "String",
+            example = "Kronik Yayınları")
     @NotNull(message = " must not be empty brand")
     private String brand;
 
+
+    @Schema(
+            description = "Author of the Book",
+            name = "author",
+            type = "String",
+            example = "İlber Ortaylı")
     @NotNull(message = " must not be empty author")
     private String author;
 
+
+    @Schema(
+            description = "Category of the Book",
+            name = "category",
+            type = "String",
+            example = "KİŞİSEL GELİŞİM")
     @NotNull(message = " must not be empty category")
     private String category;
 
+
+    @Schema(
+            description = "Number Of Pages of the Book",
+            name = "numberOfPages",
+            type = "long",
+            example = "320")
     @NotNull(message = " must not be empty Number Of Pages")
-    @Min(value = 0,message = "must be greater than or equal to zero")
+    @Min(value = 0, message = "must be greater than or equal to zero")
     private Long numberOfPages;
 
+
+    @Schema(
+            description = "Amount Of Stock of the Book",
+            name = "amountOfStock",
+            type = "long",
+            example = "10")
     @NotNull(message = " must not be empty Amount Of Stock")
-    @Min(value = 0,message = "must be greater than or equal to zero")
+    @Min(value = 0, message = "must be greater than or equal to zero")
     private Long amountOfStock;
 
+
+    @Schema(
+            description = "Purchase Price of the Book",
+            name = "purchasePrice",
+            type = "double",
+            example = "60")
     @NotNull(message = " must not be empty Purchase Price")
-    @Min(value = 0,message = "the lowest purchase price should be zero")
+    @Min(value = 0, message = "the lowest purchase price should be zero")
     private double purchasePrice;
 
+
+    @Schema(
+            description = "Sale Price of the Book",
+            name = "salePrice",
+            type = "double",
+            example = "140")
     @NotNull(message = " must not be empty Sale Price")
-    @Min(value = 0,message = "the lowest sale price should be zero")
+    @Min(value = 0, message = "the lowest sale price should be zero")
     private double salePrice;
 
+
+    @Schema(
+            description = "Discount Rate of the Book",
+            name = "discountRate",
+            type = "double",
+            example = "33")
     @NotNull(message = " must not be empty Discount Rate")
-    @Min(value = 0,message = "the lowest discount rate should be zero")
+    @Min(value = 0, message = "the lowest discount rate should be zero")
     private double discountRate;
 
+
+    @Schema(
+            description = "Discounted Price the Book",
+            name = "discountedPrice",
+            type = "double",
+            example = "1")
     @NotNull(message = " must not be empty Discounted Price")
-    @Min(value = 0,message = "the lowest discounted price rate should be zero")
+    @Min(value = 0, message = "the lowest discounted price rate should be zero")
     private double discountedPrice;
 
     public double getDiscountedPrice() {
